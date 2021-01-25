d0b65fec4008fc607803b110070b63247e70a158
“Joypham” <phamhonghanh120293@gmail.com>
2021-01-25 17:07:09 +0700

    joy xinh

--- tools/daily_user_collect_from_youtube.py
+++ tools/daily_user_collect_from_youtube.py
@@ -22,7 +22,7 @@ def daily_user_collect_from_youtube():
     # Input_url 'https://docs.google.com/spreadsheets/d/1vlMsEjwBWuuxXecadJsEbBFeuVFAHZSbOz90JhXgioo/edit#gid=1088561556'
     gsheet_id = '1vlMsEjwBWuuxXecadJsEbBFeuVFAHZSbOz90JhXgioo'
     sheet_name = 'Sheet1'
-    new_title = f"Daily contribution {date.today()} joy xinh.com"
+    new_title = f"Daily contribution {date.today()}"
     print(new_title)
 
     # PROCESS HERE:
