2f07c3731cb87552962ed311a21c6d9f82024081
“Joypham” <phamhonghanh120293@gmail.com>
2021-02-04 11:04:17 +0700

    joy xinh

--- tools/daily_export_artist_contribution.py
+++ tools/daily_export_artist_contribution.py
@@ -15,18 +15,28 @@ def export_artist_contribution():
                        & (df.Itunes_ID != 'Itunes_ID')
                        & (df.Itunes_ID.notnull())
                        ]['Itunes_ID'].drop_duplicates(keep='first').tolist()
-    result = get_df_from_query(get_artists_from_album_ituneid(album_ituneid))
+    result = get_df_from_query(get_artists_from_album_ituneid(album_ituneid))[["name", "external_id"]]
+
+    # Delete_data_cloumn_EF
+    df2 = get_df_from_speadsheet('1zamfPcMKhk0tDjqAStA3cfS4BYiNAIZicoQJHnERRd0', 'Accumulated User Contribution')
+    df2["name"] = ""
+    df2["external_id"] = ""
+    df_update_value = df2[["name", "external_id"]].values.tolist()
+    update_value(df_update_value, "Accumulated User Contribution!E2", "1zamfPcMKhk0tDjqAStA3cfS4BYiNAIZicoQJHnERRd0")
 
     # Update data to gsheet_id
-    list_result = result[['']].values.tolist()
+    list_result = result.values.tolist()
     update_value(list_result=list_result, range_to_update="Accumulated User Contribution!E2",
                  gsheet_id="1zamfPcMKhk0tDjqAStA3cfS4BYiNAIZicoQJHnERRd0")
 
 
 if __name__ == "__main__":
     start_time = time.time()
+    pd.set_option("display.max_rows", None, "display.max_columns", 60, 'display.width', 1000)
     # INPUT HERE
     # 'https://docs.google.com/spreadsheets/d/1Ck9O771xM7VArdaYxbHTVtp4kRtHzPn57EDDId0cHJc/edit#gid=0'
     # 'https://docs.google.com/spreadsheets/d/1wMLmbaY3ZJiPDU2uekzfVVLjwHOzGJ2TpYFNCWTNqAE/edit#gid=0'
+
     export_artist_contribution()
+
     print("--- %s seconds ---" % (time.time() - start_time))
